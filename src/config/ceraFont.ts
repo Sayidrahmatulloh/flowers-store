526ebb65ac01311bf264e45201d6dc572c3cb35d
Sayidrahmatulloh <sayidrahmatulloh@gmail.com>
2024-04-21 10:47:12 +0500

    add local font | cera pro

---
+++ src/config/ceraFont.ts
@@ -0,0 +1,65 @@
+import localFont from 'next/font/local'
+export const ceraFont = localFont({
+    src: [
+        {
+            path: 'FONTSPRINGDEMO-CeraProBlackItalic.woff2',
+            weight: '900',
+            style: 'italic',
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProBlackRegular.woff2',
+            weight: '900',
+            style: 'normal',
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProBold.woff2',
+            weight: 'bold',
+            style: 'normal',
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProBoldItalic.woff2',
+            weight: 'bold',
+            style: 'italic',
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProItalic.woff2',
+            weight: 'normal',
+            style: 'italic'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProLightItalic.woff2',
+            weight: '300',
+            style: 'italic'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProLightRegular.woff2',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProMediumItalic.woff2',
+            weight: '500',
+            style: 'italic'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProMediumRegular.woff2',
+            weight: '500',
+            style: 'normal'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProRegular.woff2',
+            weight: 'normal',
+            style: 'normal'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProThinItalic.woff2',
+            weight: '100',
+            style: 'italic'
+        },
+        {
+            path: 'FONTSPRINGDEMO-CeraProThinRegular.woff2',
+            weight: '100',
+            style: 'normal'
+        }
+    ]
+})
