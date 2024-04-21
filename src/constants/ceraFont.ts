0d394c5686a83093cebb1fb782e313a903ea80c7
Sayidrahmatulloh <sayidrahmatulloh@gmail.com>
2024-04-21 23:43:21 +0500

    husky & prettier & eslint config

--- src/constants/ceraFont.ts
+++ src/constants/ceraFont.ts
@@ -1,65 +1,65 @@
 import localFont from 'next/font/local'
 export const ceraFont = localFont({
-    src: [
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProBlackItalic.woff2',
-            weight: '900',
-            style: 'italic',
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProBlackRegular.woff2',
-            weight: '900',
-            style: 'normal',
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProBold.woff2',
-            weight: 'bold',
-            style: 'normal',
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProBoldItalic.woff2',
-            weight: 'bold',
-            style: 'italic',
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProItalic.woff2',
-            weight: 'normal',
-            style: 'italic'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProLightItalic.woff2',
-            weight: '300',
-            style: 'italic'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProLightRegular.woff2',
-            weight: '300',
-            style: 'normal'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProMediumItalic.woff2',
-            weight: '500',
-            style: 'italic'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProMediumRegular.woff2',
-            weight: '500',
-            style: 'normal'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProRegular.woff2',
-            weight: 'normal',
-            style: 'normal'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProThinItalic.woff2',
-            weight: '100',
-            style: 'italic'
-        },
-        {
-            path: '../fonts/FONTSPRINGDEMO-CeraProThinRegular.woff2',
-            weight: '100',
-            style: 'normal'
-        }
-    ]
+  src: [
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProBlackItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProBlackRegular.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProBold.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProBoldItalic.woff2',
+      weight: 'bold',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProItalic.woff2',
+      weight: 'normal',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProLightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProLightRegular.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProMediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProMediumRegular.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProRegular.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProThinItalic.woff2',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/FONTSPRINGDEMO-CeraProThinRegular.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
 })
